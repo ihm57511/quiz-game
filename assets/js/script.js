be8ac32c3f3ed63df6d4d76d227c91cf314e12ce
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-02-18 17:10:27 -0500

    starting from new direction, setting js variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,69 @@
-var timerEl = document.getElementById('timer');
-var alert = document.getElementById('alert');
-var card = document.getElementById('card');
+var questions = [
+    {question: "The condition in an if / else statement is enclosed within ________?",
+        answers: [
+            {answer: "quotes", isCorrect: false},
+            {answer: "curly brackets", isCorrect: false},
+            {answer: "parentheses", isCorrect: true},
+            {answer: "square brackets", isCorrect: false},
+        ]
+    },
+    {question: "Commonly used data types DO NOT include:",
+        answers: [
+            {answer: "strings", isCorrect: false},
+            {answer: "booleans", isCorrect: false},
+            {answer: "alerts", isCorrect: true},
+            {answer: "numbers", isCorrect: false},
+        ]
+    },
+    {question: "String values must be enclosed within ________ when being assigned to variables.",
+        answers: [
+            {answer: "commas", isCorrect: false},
+            {answer: "curly brackets", isCorrect: false},
+            {answer: "quotes", isCorrect: true},
+            {answer: "parentheses", isCorrect: false},
+        ]
+    },
+    {question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            {answer: "JavaScript", isCorrect: false},
+            {answer: "terminal/bash", isCorrect: false},
+            {answer: "for loops", isCorrect: false},
+            {answer: "console.log", isCorrect: true},
+        ]
+    },
+    {question: "Arrays in JavaScript can be used to store __________.",
+        answers: [
+            {answer: "numbers and strings", isCorrect: false},
+            {answer: "other arrays", isCorrect: false},
+            {answer: "booleans", isCorrect: false},
+            {answer: "all of the above", isCorrect: true},
+        ]
+    },
 
+        
+]
+
+    
+    
+
+function quiz() {
+    function setTime() {
+        // start time and set interval
+        function display() {
+            for (i=0, i < questions.length, i++)
+            
+            if ("clicked" === true) {
+            
+            }
+        }
+    }
+}
+// need function for cards
+// start-card visible at load,
+// at start button press, display change to none,
+// card 1 then set to visible, 
+// if button class correct pressed then { card display change to none, 
+// next card display change to visible} 
+// else {card display change to none, 
+// next card display change to visible, add 10 sec penalty }
+// need form for score submision
